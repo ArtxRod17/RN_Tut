902f4210c072f3bd98d522a5853db030c487b902
ArtxRod17 <Arturoxrodriguez17@gmail.com>
2025-02-14 19:54:36 -0700

    added not found file

--- StickerSmash/app/index.tsx
+++ StickerSmash/app/index.tsx
@@ -1,11 +1,13 @@
 import { Text, View, StyleSheet } from "react-native";
+import { Link } from 'expo-router'
 
 export default function Index() {
   return (
     <View
       style={styles.container}
     >
-      <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.text}>Home Screen</Text>
+      <Link href="/about" style={styles.button}>Go to About Screen</Link>
     </View>
   );
 }
@@ -20,5 +22,10 @@ const styles = StyleSheet.create( {
   },
   text: {
     color: 'white'
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: 'red'
   },
 });
